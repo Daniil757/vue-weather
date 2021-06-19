06871e6afe4107a332b0ca54ed313a0ddcaf294b
Daniil757 <77358905+Daniil757@users.noreply.github.com>
2021-06-19 18:59:43 +0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2bf7b2ec23bec072b2912c6acc9079b8.js"
+  "./precache-manifest.2bf7b2ec23bec072b2912c6acc9079b8.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "vue-weather"});
