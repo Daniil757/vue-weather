937aa7c9e6e1d1114eb85218d903497c15fa2034
Daniil757 <77358905+Daniil757@users.noreply.github.com>
2021-06-19 15:54:37 +0400

    Add files via upload

---
+++ precache-manifest.b3882bdcc99f9d529a973658e9153fd6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0c40d7bb024dde077b5",
+    "url": "/css/app.599e004a.css"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/img/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/img/warm-bg.635ed87d.jpg"
+  },
+  {
+    "revision": "a2ab3c14e70922650b2bf674528828f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0c40d7bb024dde077b5",
+    "url": "/js/app.baeb3544.js"
+  },
+  {
+    "revision": "a676a97dde04d5159b2b",
+    "url": "/js/chunk-vendors.6637e80a.js"
+  },
+  {
+    "revision": "abb7651b3fb33cbb939069d3e6d6a7cc",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
